c3b1660d8904d9c9813644546c145d2967ac3239
Andrei Robu <andreidrobu@gmail.com>
2026-02-03 16:06:32 +0200

    fix URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://a-robu.github.io/personal-site",
-  base: "/personal-site",
+  site: "https://a-robu.github.io/portfolio-site",
+  base: "/portfolio-site",
 });
