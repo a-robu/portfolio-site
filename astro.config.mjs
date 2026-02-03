91964d6adba970775e3d0328dd713b91c683653f
Andrei Robu <andreidrobu@gmail.com>
2026-02-03 16:37:13 +0200

    assume base path is /

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://a-robu.github.io/portfolio-site",
-  base: "/portfolio-site",
+  site: "https://andreirobu.uk",
 });
